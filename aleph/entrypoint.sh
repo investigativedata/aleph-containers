f0ed51086e13756f7fb0b43ffc536c69a51624ba
Simon Wörpel <simon.woerpel@pm.me>
2023-09-19 11:14:57 +0200

    ✨ (aleph) add custom entrypoint

---
+++ aleph/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ${ALEPH_PAGES_ZIP+x} ]]; then
+  curl -L -o /tmp/aleph-pages.zip "$ALEPH_PAGES_ZIP"
+  unzip -o /tmp/aleph-pages.zip -d /tmp/aleph-pages/
+  rm -rf /aleph/aleph/pages/*
+  mv /tmp/aleph-pages/**/*.md /aleph/aleph/pages/
+fi
+
+exec $@
